7586c98d6509b5f9fd4d4584425a2f86778b3546
Yury <mr.Shapovaloff3101@gmail.com>
2022-05-04 17:59:59 +0300

    module1-task3

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -2,14 +2,17 @@ import throttle from 'lodash/throttle';
 
 export default class FullPageScroll {
   constructor() {
-    this.THROTTLE_TIMEOUT = 1000;
-    this.scrollFlag = true;
-    this.timeout = null;
+    this.THROTTLE_TIMEOUT = 2000;
 
     this.screenElements = document.querySelectorAll(`.screen:not(.screen--result)`);
     this.menuElements = document.querySelectorAll(`.page-header__menu .js-menu-link`);
+    this.animationBar = document.querySelector(`.animation-bar`);
+
+    this.storyScreenIndex = 1;
+    this.prizeScreenIndex = 2;
 
     this.activeScreen = 0;
+    this.previousScreen = 0;
     this.onScrollHandler = this.onScroll.bind(this);
     this.onUrlHashChengedHandler = this.onUrlHashChanged.bind(this);
   }
@@ -22,25 +25,16 @@ export default class FullPageScroll {
   }
 
   onScroll(evt) {
-    if (this.scrollFlag) {
-      this.reCalculateActiveScreenPosition(evt.deltaY);
-      const currentPosition = this.activeScreen;
-      if (currentPosition !== this.activeScreen) {
-        this.changePageDisplay();
-      }
+    const currentPosition = this.activeScreen;
+    this.reCalculateActiveScreenPosition(evt.deltaY);
+    if (currentPosition !== this.activeScreen) {
+      this.changePageDisplay();
     }
-    this.scrollFlag = false;
-    if (this.timeout !== null) {
-      clearTimeout(this.timeout);
-    }
-    this.timeout = setTimeout(() => {
-      this.timeout = null;
-      this.scrollFlag = true;
-    }, this.THROTTLE_TIMEOUT);
   }
 
   onUrlHashChanged() {
     const newIndex = Array.from(this.screenElements).findIndex((screen) => location.hash.slice(1) === screen.id);
+    this.previousScreen = this.activeScreen;
     this.activeScreen = (newIndex < 0) ? 0 : newIndex;
     this.changePageDisplay();
   }
@@ -52,14 +46,14 @@ export default class FullPageScroll {
   }
 
   changeVisibilityDisplay() {
-    this.screenElements.forEach((screen) => {
-      screen.classList.add(`screen--hidden`);
-      screen.classList.remove(`active`);
-    });
-    this.screenElements[this.activeScreen].classList.remove(`screen--hidden`);
-    setTimeout(() => {
+    if (this.previousScreen === this.storyScreenIndex && this.activeScreen === this.prizeScreenIndex) {
+      this.screenElements[this.previousScreen].classList.add(`transtioning`);
+    }
+
+    if (!(this.previousScreen === this.storyScreenIndex && this.activeScreen === this.prizeScreenIndex)) {
+      this.screenElements[this.activeScreen].classList.remove(`screen--hidden`);
       this.screenElements[this.activeScreen].classList.add(`active`);
-    }, 100);
+    }
   }
 
   changeActiveMenuItem() {
